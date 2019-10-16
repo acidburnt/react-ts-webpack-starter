7797d95aa4427a1a07a18a597c52cc468c15a15f
michał szydłowski <locky700@gmail.com>
2019-10-16 23:53:44 +0200

    Setup react with typescript

---
+++ src/Root.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Root = () => (
+  <>
+    <h1>Hello</h1>
+    <p>is this working?</p>
+  </>
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+};
